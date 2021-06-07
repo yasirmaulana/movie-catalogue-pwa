1e4827a16804aef7286aab987d00154eb9eb51bb
yasirmaulana <yasir.maulana@gmail.com>
2021-06-07 23:26:10 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'f5cac66d5ff96a61424f69d3dfe6757d',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
