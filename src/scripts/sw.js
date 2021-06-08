04362f1c26801b16d81809d5234f00bd131f2ea1
yasirmaulana <yasir.maulana@gmail.com>
2021-06-08 16:13:45 +0700

    add service worker

---
+++ src/scripts/sw.js
@@ -0,0 +1,13 @@
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(CacheHelper.deleteOldCache());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(CacheHelper.revalidateCache(event.request));
+});
